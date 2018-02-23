2bc372f0866def185bb95a168f587e28fee94214
gustavopimentelrinaldi <gustavorinaldiiii@gmail.com>
2018-02-23 18:27:47 -0300

    initialized

---
+++ config/server.js
@@ -0,0 +1,25 @@
+/* importar o módulo do framework express */
+var express = require('express');
+
+/* importar o módulo do consign */
+var consign = require('consign');
+
+/* iniciar o objeto do express */
+var app = express();
+
+/* setar as variáveis 'view engine' e 'views' do express */
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+/* configurar o middleware express.static */
+app.use(express.static('./app/public'));
+
+/* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
+consign()
+	.include('app/routes')
+	.then('app/models')
+	.then('app/controllers')
+	.into(app);
+
+/* exportar o objeto app */
+module.exports = app;
